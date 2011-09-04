faf6ccbb7194b9713da2182a42559e4f2ff067b1
Sam Stokes <me@samstokes.co.uk>
2011-09-04 15:18:08 +0100

    Basic implementation: create, list, complete, uncomplete, delete tasks

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE TemplateHaskell, QuasiQuotes, OverloadedStrings #-}
 module Handler.Root where
 
+import Control.Applicative
+import Data.Text (Text)
+import Yesod.Auth (maybeAuthId)
 import Foundation
 
 -- This is a handler function for the GET request method on the RootR
@@ -13,7 +16,62 @@ import Foundation
 getRootR :: Handler RepHtml
 getRootR = do
     mu <- maybeAuth
+    let uid = fmap fst mu
+    tasks <- maybe (return []) (runDB . userTasks) uid
+    ((_, taskWidget), _) <- maybe (return ((undefined, [whamlet|""|]), undefined)) (generateFormPost . taskForm) uid
     defaultLayout $ do
         h2id <- lift newIdent
-        setTitle "yesodoro homepage"
+        setTitle "yesodoro"
         addWidget $(widgetFile "homepage")
+  where
+    userTasks userId = selectList [TaskUser ==. userId] [Asc TaskDone]
+
+oneButton :: Text -> YesodoroRoute -> Widget
+oneButton label route = [whamlet|
+  <form method=POST action=@{route}
+    <input type=submit value=#{label}
+|]
+
+taskForm :: UserId -> Html -> Form Yesodoro Yesodoro (FormResult Task, Widget)
+taskForm uid = renderDivs $ Task uid
+  <$> areq textField "Title" Nothing
+  <*> pure 0
+  <*> pure False
+
+postTasksR :: Handler RepHtml
+postTasksR = do
+  uid <- maybeAuthId
+  ((result, taskWidget), _) <- maybe (return ((FormMissing, [whamlet|""|]), undefined)) (runFormPost . taskForm) uid
+  case result of
+    FormSuccess task -> do
+      runDB $ insert task
+      redirect RedirectTemporary RootR
+    _ -> undefined
+
+
+postCompleteTaskR :: TaskId -> Handler RepHtml
+postCompleteTaskR = setTaskDoneness True
+
+postRestartTaskR :: TaskId -> Handler RepHtml
+postRestartTaskR = setTaskDoneness False
+
+setTaskDoneness :: Bool -> TaskId -> Handler RepHtml
+setTaskDoneness done taskId = do
+  runDB $ update taskId [TaskDone =. done]
+  redirect RedirectTemporary RootR
+
+setTaskDonenessRoute :: (TaskId, Task) -> YesodoroRoute
+setTaskDonenessRoute (taskId, task) = route taskId
+  where route | taskDone task = RestartTaskR
+              | otherwise     = CompleteTaskR
+
+setTaskDonenessButton :: (TaskId, Task) -> Widget
+setTaskDonenessButton (taskId, task) = oneButton action (route taskId)
+  where action = taskActionName task
+        route | taskDone task = RestartTaskR
+              | otherwise     = CompleteTaskR
+
+postDeleteTaskR :: TaskId -> Handler RepHtml
+postDeleteTaskR taskId = do
+  runDB $ delete taskId
+  redirect RedirectTemporary RootR
--- Model.hs
+++ Model.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, GADTs #-}
+{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, GADTs, OverloadedStrings #-}
 module Model where
 
 import Yesod
+import Data.String (IsString)
 import Data.Text (Text)
+import Text.Blaze (ToHtml)
 
 
 -- You can define all of your database entities in the entities file.
@@ -10,3 +12,14 @@ import Data.Text (Text)
 -- at:
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] $(persistFile "config/models")
+
+
+newtype TaskState = TaskState Text
+  deriving (ToHtml, IsString)
+
+taskState :: Task -> TaskState
+taskState task = if taskDone task then "done" else "pending"
+
+taskActionName :: Task -> Text
+taskActionName task | taskDone task = "Restart"
+                    | otherwise     = "Complete"
